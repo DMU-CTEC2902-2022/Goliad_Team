0e21cc757ea2d733c29f85831bc4e1c9d4996c89
Dylan Wega <dymasan@yahoo.fr>
2022-04-13 23:23:18 +0100

    Test class & student class added

---
+++ infoTechCollege.Tests/Models/studentModelTest.cs
@@ -0,0 +1,99 @@
+﻿using infoTechCollege.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace infoTechCollege.Tests.Models
+{
+    [TestClass]
+    public class studentModelTest
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //creating an instance of a student
+            clsStudent student = new clsStudent();
+            //test to see if it works
+            Assert.IsNotNull(student);
+        }
+
+
+
+        [TestMethod]
+        public void firstname()
+        {
+            //creating an instance of a student
+            clsStudent student = new clsStudent();
+            //declaring test data
+            string testdata = "Paul";
+            //assigning data to the property
+            student.firstname = testdata;
+            //test to see if it works
+            Assert.AreEqual(testdata, student.firstname);
+        }
+
+        [TestMethod]
+        public void lastname()
+        {
+            //creating an instance of a student
+            clsStudent student = new clsStudent();
+            //declaring test data
+            string testdata = "Banner";
+            //assigning data to the property
+            student.lastname = testdata;
+            //test to see if it works
+            Assert.AreEqual(testdata, student.lastname);
+        }
+
+        [TestMethod]
+        public void email()
+        {
+            //creating an instance of a student
+            clsStudent student = new clsStudent();
+            //declaring test data
+            string testdata = "paul@gmail.com";
+            //assigning data to the property
+            student.email = testdata;
+            //test to see if it works
+            Assert.AreEqual(testdata, student.email);
+        }
+
+        [TestMethod]
+        public void town()
+        {
+            //creating an instance of a student
+            clsStudent student = new clsStudent();
+            //declaring test data
+            string testdata = "Leicester";
+            //assigning data to the property
+            student.town = testdata;
+            //test to see if it works
+            Assert.AreEqual(testdata, student.town);
+        }
+
+        [TestMethod]
+        public void dateOfbirth()
+        {
+            //creating an instance of a student
+            clsStudent student = new clsStudent();
+            //declaring test data
+            string testdata = "12/02/1998";
+            //assigning data to the property
+            student.dateOfBirth = testdata;
+            //test to see if it works
+            Assert.AreEqual(testdata, student.dateOfBirth);
+        }
+
+        [TestMethod]
+        public void phoneNumber()
+        {
+            //creating an instance of a student
+            clsStudent student = new clsStudent();
+            //declaring test data
+            string testdata = "123456789014";
+            //assigning data to the property
+            student.phonemumber = testdata;
+            //test to see if it works
+            Assert.AreEqual(testdata, student.phonemumber);
+        }
+    }
+}
---
+++ infoTechCollege/Models/clsStudent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace infoTechCollege.Models
+{
+    public class clsStudent
+    {
+        private string firstname;
+        private string lastname;
+        private string email;
+        private string town;
+        private string dateOfBirth;
+        private string phonemumber;
+
+        public string Firstname { get => firstname; set => firstname = value; }
+        public string Lastname { get => lastname; set => lastname = value; }
+        public string Email { get => email; set => email = value; }
+        public string Town { get => town; set => town = value; }
+        public string DateOfBirth { get => dateOfBirth; set => dateOfBirth = value; }
+        public string Phonemumber { get => phonemumber; set => phonemumber = value; }
+    }
+}
