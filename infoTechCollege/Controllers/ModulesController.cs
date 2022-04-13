287037033a1a693b09ac1f24e071851b1c9df98a
Dylan Wega <dymasan@yahoo.fr>
2022-04-13 21:29:48 +0100

    views & controllers for modules added

---
+++ infoTechCollege/Controllers/ModulesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace infoTechCollege.Controllers
+{
+    public class ModulesController : Controller
+    {
+        // GET: Modules
+        public ActionResult ComputerScience()
+        {
+            return View();
+        }
+
+        public ActionResult SoftwareEngineering()
+        {
+            return View();
+        }
+    }
+}
