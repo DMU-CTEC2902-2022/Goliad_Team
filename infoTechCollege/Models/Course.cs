542c45f57cd23b81ece820bc46d5f24ef80c3799
ViolaManzini <p15209829@my365.dmu.ac.uk>
2022-04-14 15:37:06 +0100

    updating layout

---
+++ infoTechCollege/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace infoTechCollege.Models
+{
+    public class Course
+    { 
+        public int Code { get; set; }
+        public string Title { get; set; }
+    }
+}
