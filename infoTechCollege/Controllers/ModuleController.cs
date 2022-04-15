ef17956213b93ae1604ea8dcbbf9e0de5bea0227
ViolaManzini <p15209829@my365.dmu.ac.uk>
2022-04-15 04:58:32 +0100

    fixing link issue from course list - not complete

---
+++ infoTechCollege/Controllers/ModuleController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace infoTechCollege.Controllers
+{
+    public class ModuleController : Controller
+    {
+        // GET: Module
+        public ActionResult ComputerScience()
+        {
+            return View();
+        }
+
+        public ActionResult SoftwareEngineering()
+        {
+            return View();
+        }
+
+        public ActionResult CyberSecurity()
+        {
+            return View();
+        }
+    }
+}
